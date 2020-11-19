f592d763ab03ff5619a682e1a03c5ef8863b84b8
Gurdeepak Sidhu <gurdeepaksidhu@yahoo.com>
2020-11-18 18:51:39 -0800

    Added main function in demo.R

--- src/demo.R
+++ src/demo.R
@@ -9,10 +9,15 @@ Options:
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 [--arg4=<arg4>]   Takes any value (this is an optional option)
-"-> doc
+" -> doc
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg4)
+
+main <- function(list, var4) {
+  print(list)
+  print(typeof(list))
+  print(var4)
+}
+  
+main(opt, opt$arg4)
